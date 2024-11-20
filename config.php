7c87791e1d03f8aacfc75139b9fcd399fea77a70
Nicole Espiritu <146156901+143nicolay@users.noreply.github.com>
2024-11-20 21:02:24 +0800

    config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_SERVERNAME', "localhost");
+define('DB_USERNAME', "nicole");
+define('DB_PASSWORD', "12345");
+define('DB_NAME', "demo");
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
